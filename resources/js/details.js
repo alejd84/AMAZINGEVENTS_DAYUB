ebfcf5d38c7f814617f9224df0b2ebfabd12fb8c
alejd84 <dayubalexis@gmail.com>
2023-09-01 15:11:33 -0300

    Entrega Task3

---
+++ resources/js/details.js
@@ -0,0 +1,104 @@
+const containerDetails = document.getElementById("DetailedCards");
+
+let eventsData = data.events
+let id = location.search.slice(4)
+let eventDetails = eventsData.filter(event => id == event._id)
+eventDetails = eventDetails[0]
+
+createCardDetail(eventDetails)
+
+function createCardDetail(event) {
+    let AorE
+    if (event.assistance !== undefined) {
+        AorE = ['Assistance', event.assistance]
+    } else {
+        AorE = ['Estimate', event.estimate]
+    }
+    containerDetails.innerHTML = `
+    <div class="card text-bg-dark mb-3 col-12 col-lg-10 shadow" id="nocardshadowmoveid">
+        <div class="row g-3">
+            <div class="col-md-4">
+                <img src="${event.image}" class="img-fluid rounded-start"
+                    alt="${event.name}">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                </div>
+                <div class="card-footer text-muted">
+                    <ul
+                        class="list-group list-group-flush d-flex flex-row flex-wrap justify-content-around">
+                        <li
+                            class="list-group-item d-flex align-items-center justify-content-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-grid" viewBox="0 0 16 16">
+                                <path
+                                    d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
+                            </svg>
+                            &nbsp; Category: ${event.category}
+                        </li>
+                        <li
+                            class="list-group-item d-flex align-items-center justify-content-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-calendar-date" viewBox="0 0 16 16">
+                                <path
+                                    d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
+                                <path
+                                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+                            </svg>
+                            &nbsp; Date: ${event.date}
+                        </li>
+                        <li
+                            class="list-group-item d-flex align-items-center justify-content-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                            </svg>
+                            &nbsp; Place: ${event.place}
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-footer text-muted">
+                    <ul
+                    class="list-group list-group-flush d-flex flex-row flex-wrap justify-content-around">
+                        <li
+                            class="list-group-item d-flex align-items-center justify-content-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-door-closed" viewBox="0 0 16 16">
+                                <path
+                                    d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z" />
+                                <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z" />
+                            </svg>
+                            &nbsp; Capacity: ${event.capacity}
+                        </li>
+                        <li
+                            class="list-group-item d-flex align-items-center justify-content-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                        </svg>
+                            &nbsp; ${AorE[0]} : ${AorE[1]}
+                        </li>
+                        <li
+                            class="list-group-item d-flex align-items-center justify-content-between">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-tag" viewBox="0 0 16 16">
+                                <path
+                                    d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
+                                <path
+                                    d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
+                            </svg>
+                            &nbsp; Price: ${event.price}
+                        </li>
+                    </ul>
+                </div>
+                <div class="detailsBtns d-flex flex-row justify-content-around align-items-center">
+                    <a class="btn btn-outline-secondary detailsClass m-3" href="#" onclick="history.back();" aria-label="Go Back"><span>Go Back</span></a>
+                    <a class="btn btn-outline-success detailsClasss m-3" href="#" aria-label="Buy Ticket"><span>Buy Ticket</span></a>
+                </div>
+            </div>
+        </div>
+    </div>    
+    `
+}
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,25 +1,121 @@
-let currentEventsCards = [];
+const containerCheckbox = document.getElementById('checkboxes');
+const containerCards = document.getElementById("currentCards");
+const containernoresultsCards = document.getElementById("noresultsCards");
+const title = document.getElementById("title");
+const searchId = document.getElementById("searchId");
 
-let currentCard = document.getElementById("currentCards")
+async function getFullEvents() {
+    const eventsDate = data.currentDate
+    const eventsData = data.events
 
-for(let event of data.events){
-    currentEventsCards += 
-    `
-    <div class="col mb-4">
-        <div class="card h-100 text-bg-dark">
-            <div class="card-header">
-                ${event.place}
-            </div>        
-            <img src="${event.image}" class="card-img-top" style="height:10rem">
-            <div class="card-body">
-                <h5 class="card-title">${event.name}</h5>
-                <p class="card-text">${event.description}</p>
-            </div>    
-            <div class="card-footer d-flex justify-content-between"> 
-                <h6><b>Date: </b>${event.date}</h6>     
-                <a href="./details.html" class="btn btn-outline-danger">Details ></a>
-            </div>
-        </div>
+    const eventsComplete = eventsData.map((element) => element)
+
+    const eventsHome = eventsComplete.filter(() => title.text.includes('Home'))
+    const eventsUpcoming = eventsComplete.filter(() => title.text.includes('Upcoming')).filter(element => element.date > eventsDate)
+    const eventsPast = eventsComplete.filter(() => title.text.includes('Past')).filter(element => element.date < eventsDate)
+
+    let fullEvents = [...eventsHome, ...eventsUpcoming, ...eventsPast]
+    fullEvents.forEach(createCard)
+
+    const categories = eventsComplete.reduce((allCategories, event) => Array.from(new Set([...allCategories, event.category])), [])
+
+    categories.forEach(createCheckbox)
+
+    let checkBoxId = Array.from(document.querySelectorAll('.checkId'))
+    checkBoxId.forEach(checkbox => checkbox.addEventListener("click", filterCheckCards))
+
+    searchId.addEventListener('input', filterCheckCards)
+
+    function filterCheckCards() {
+        let filteredChecks = checkEvents(fullEvents)
+        let filteredSearch = filterCardsBySearch(filteredChecks, searchId.value)
+        if (filteredSearch.length !== 0) {
+            containerCards.innerHTML = ``
+            containernoresultsCards.innerHTML = ``
+        }
+        filteredSearch.forEach(createCard)
+    }
+
+    function checkEvents(array) {
+        let checkboxChecked = checkBoxId.filter(check => check.checked).map(checkCategory => checkCategory.value)
+        if (checkboxChecked.length > 0) {
+            let filteredCheckBox = array.filter(event => checkboxChecked.includes(event.category))
+            return filteredCheckBox
+        }
+        return array
+    }
+}
+
+function createCheckbox(category) {
+    let label = document.createElement("label")
+    label.classList.add("d-flex", "align-items-center", "p-1")
+
+    let input = document.createElement("input")
+    input.type = "checkbox"
+    input.classList.add("checkboxes", "checkId", "custom-checkbox", "categories")
+    input.checked = true
+    input.name = category
+    input.value = category
+    input.id = category
+    input.setAttribute("aria-label", category)
+
+    let span = document.createElement("span")
+    span.textContent = ">" + category
+
+    label.appendChild(input)
+    label.appendChild(span)
+    containerCheckbox.appendChild(label)
+}
+
+function filterCardsBySearch(array, texto) {
+    let cardsFilterForSearch = array.filter(event => event.name.toLowerCase().includes(texto.toLowerCase()));
+    if (cardsFilterForSearch.length === 0) {
+        searchEmpty()
+        return []
+    }
+    return cardsFilterForSearch
+}
+
+function searchEmpty() {
+    containerCards.innerHTML = ``
+    containernoresultsCards.innerHTML =
+        `
+    <div class="card noresultsclass" id="noresultsmsgid" role="alert" aria-label="No results found">
+        <img src="./resources/images/main/404.png" alt="No results found">
     </div>
-    `
-} currentCard.innerHTML = currentEventsCards;
+    <a href="./index.html"" aria-label="Try again searching by categories">
+        <p>TRY AGAIN SEARCHING BY CATEGORIES</p>
+    </a>
+    `;
+}
+
+function createCard(array) {
+    containerCards.innerHTML +=
+        `
+        <div class="col mb-4">
+            <div class="card h-100 text-bg-dark">
+                <div class="card-header">
+                    ${array.place}
+                </div>        
+                <img src="${array.image}" class="card-img-top" style="height:10rem" alt="${array.name}">
+                <div class="card-body text-start pt-1">
+                    <p class="card-text fs-6 p-0 mb-1 text-secondary">${array.category}</p>
+                    <h5 class="card-title">${array.name}</h5>
+                    <p class="card-text card-descr fs-6">${array.description}</p>
+                </div>
+                <div class="card-footer pt-3 pb-3 d-flex justify-content-between align-items-center align-items-xl-baseline">
+                    <p class="mb-0 d-flex flex-row flex-md-column flex-xl-row">
+                        <span>
+                            <i class="bi bi-calendar3-event" aria-hidden="true"></i>
+                            <span class="visually-hidden">Date:</span>&nbsp;
+                        </span>
+                        ${array.date}
+                    </p>
+                    <a class="btn btn-outline-danger detailsClass" href="./details.html?id=${array._id}" aria-label="Details for ${array.name}"><span>Details</span></a>
+                </div>
+            </div>
+        </div>  
+    `;
+}
+
+getFullEvents()
